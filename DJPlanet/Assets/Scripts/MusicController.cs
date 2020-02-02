2f961056398d4e9ada819a2309f339f3c3405ac7
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2020-02-02 07:53:40 -0500

    Audio controllers and scratch sounds

---
+++ DJPlanet/Assets/Scripts/MusicController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+/**
+ * Use this script on AudioSource object which has main audioclip attached
+ */
+public class MusicController : MonoBehaviour
+{
+    AudioSource musicAudio;
+
+    //Play the music
+    bool shouldPlayMusic;
+
+    void Start()
+    {
+        //Fetch the AudioSource from the GameObject
+        musicAudio = GetComponent<AudioSource>();
+        //Ensure the toggle is set to true for the music to play at start-up
+        shouldPlayMusic = true;
+        musicAudio.Play();
+    }
+
+    public void toggleBGMusic()
+    {
+        shouldPlayMusic = !shouldPlayMusic;
+
+        if (shouldPlayMusic)
+        {
+            //Play the audio you attach to the AudioSource component
+            musicAudio.Play();
+        } else
+        {
+            //Stop the audio
+            musicAudio.Pause();
+        }
+    }
+
+}

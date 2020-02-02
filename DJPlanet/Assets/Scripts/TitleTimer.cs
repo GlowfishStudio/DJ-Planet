65e8b1bc32c1cacc2eb87b722587c8c1c88baa0b
Magecoerlin <colin.magerle@gmail.com>
2020-02-02 14:05:43 -0500

    Added Logo and finished title screen

---
+++ DJPlanet/Assets/Scripts/TitleTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleTimer : MonoBehaviour
+{
+    public float counter;
+    public float countAmount;
+
+    public GameObject toTurnOff;
+    public GameObject toTurnOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= countAmount) {
+            toTurnOff.SetActive(false);
+            toTurnOn.SetActive(true);
+        }
+    }
+}

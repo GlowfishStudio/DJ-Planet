104428af74b46963a3d429ab07723b21a89a9a2c
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2020-02-02 13:54:43 -0500

    Merged both subprojects

--- DJPlanet/Assets/Scripts/LoadScene.cs
+++ DJPlanet/Assets/Scripts/LoadScene.cs
@@ -5,6 +5,6 @@ public class LoadScene : MonoBehaviour
 {
     public void LoadNewScene() { 
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("AndrewTest", LoadSceneMode.Single);
+        SceneManager.LoadScene("DJTest", LoadSceneMode.Single);
     }
 }
